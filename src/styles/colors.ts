ef5ed01f425c4f32783625143addb6a8d627431e
Félix Bouveret <35692793+felixbouveret@users.noreply.github.com>
2024-04-25 16:52:03 +0200

    fix: update colors and use colors.ts to export them (#3)

---
+++ src/styles/colors.ts
@@ -0,0 +1,67 @@
+// colors are automatically added as global
+// css variables in the vite.config.ts file
+
+export default {
+  "orange-150": "#792611",
+  "orange-125": "#B93C1C",
+  "orange-115": "#D0431E",
+  "orange-100": "#F24C21",
+  "orange-85": "#F5704D",
+  "orange-75": "#F7947A",
+  "orange-50": "#FAB7A6",
+  "orange-25": "#FCDBD3",
+
+  "pink-150": "#753C77",
+  "pink-125": "#B35DB5",
+  "pink-115": "#C968CB",
+  "pink-100": "#EA78ED",
+  "pink-85": "#EE93F1",
+  "pink-75": "#F2AEF4",
+  "pink-50": "#F7C9F8",
+  "pink-25": "#FBE4FB",
+
+  "purple-150": "#4F3C76",
+  "purple-125": "#795DB3",
+  "purple-115": "#8768CA",
+  "purple-100": "#9D78EB",
+  "purple-85": "#B193EF",
+  "purple-75": "#C4AEF3",
+  "purple-50": "#D8C9F7",
+  "purple-25": "#EBE4FB",
+
+  "green-150": "#30664B",
+  "green-125": "#4B9B74",
+  "green-115": "#53AE81",
+  "green-100": "#5FCB96",
+  "green-85": "#7FD5AB",
+  "green-75": "#9FE0C0",
+  "green-50": "#BFEAD5",
+  "green-25": "#DFF5EA",
+
+  "yellow-150": "#756407",
+  "yellow-125": "#B79A00",
+  "yellow-115": "#D9B600",
+  "yellow-100": "#EBC70E",
+  "yellow-85": "#EFD33E",
+  "yellow-75": "#F3DE6E",
+  "yellow-50": "#F7E99F",
+  "yellow-25": "#FBF4CF",
+
+  "cream-100": "#F7F4EF",
+  "cream-85": "#F1EDE4",
+  "cream-75": "#F1EDE4",
+  "cream-50": "#F9F8F4",
+  "cream-25": "#FCFBFA",
+
+  "black-100": "#0D0D0D",
+  "black-85": "#3D3D3D",
+  "black-75": "#6E6E6E",
+  "black-50": "#9E9E9E",
+  "black-25": "#CFCFCF",
+
+  "grey-100": "#E7E7E7",
+  "grey-85": "#ECECEC",
+  "grey-75": "#EDEDED",
+  "grey-50": "#F3F3F3",
+  "grey-25": "#F9F9F9",
+};
