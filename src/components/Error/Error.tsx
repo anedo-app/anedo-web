1c2bd4585ab72874c5337d73281e33a44ef11b46
felixbouveret <bouveret.felix@gmail.com>
2024-05-05 13:34:11 +0200

    Add error

---
+++ src/components/Error/Error.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import colors from "@/styles/colors";
+import style from "./Error.module.scss";
+import useStyles from "@/hooks/useStyles";
+import {AlertCircleIcon} from "@/Icons";
+import {ErrorProps, ErrorRolesEnum} from "./Error.types";
+
+const Error: React.FC<ErrorProps> = ({className, error}) => {
+  const {s} = useStyles();
+
+  const classes = s([className, style.container]);
+
+  return (
+    error && (
+      <span className={classes} role={ErrorRolesEnum.ERROR}>
+        <AlertCircleIcon
+          size={16}
+          color={colors["orange-100"]}
+          className={style.icon}
+        />
+        <p>{error}</p>
+      </span>
+    )
+  );
+};
+
+export default Error;
